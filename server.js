c32221c4983fbe4783daec3c9d799ee9352f5ffc
Whoffie <will@dontbestup.id>
2023-07-29 17:31:16 -0500

    Add "1" to current month for readability

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.get("/hive/", (req, res) => {
     let stmt = "INSERT INTO `data` (`temperature`, `humidity`, `date`, `time`) VALUES (?, ?, ?, ?)"
     let current = new Date()
 
-    let date = current.getMonth() + "/" + current.getDate() + "/" + current.getFullYear()
+    let date = current.getMonth() + 1 + "/" + current.getDate() + "/" + current.getFullYear()
     
     if (current.getMinutes() < 10) {
       let time = current.getHours() + ":0" + current.getMinutes()
